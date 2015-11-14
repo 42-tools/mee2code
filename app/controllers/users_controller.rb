d99ef92b265f0a17417af2279f694ca1be50f683
Jonathan <john@anatec.io>
2015-11-14 17:43:13 +0100

    Groupement des historiques par semaine

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,6 @@
 class UsersController < ApplicationController
   # GET /resource/histories
   def histories
-    @histories = current_user.user_histories
+    @histories = current_user.user_histories.order(begin_at: :desc).group_by{ |u| u.begin_at.beginning_of_week }
   end
 end
