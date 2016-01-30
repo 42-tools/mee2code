7248909cf75d4cd19ae737ae478acc1bb0bb8260
Jonathan <pretrine@hotmail.fr>
2016-01-30 14:51:59 +0100

    Ajout dans la whitelist du bypass l'ip v6 local

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   before_action :authentication
 
   def bypass_ip
-    whitelist = ['127.0.0.1/32', '62.210.32.0/24', '62.210.33.0/24', '62.210.34.0/24'].map { |v| IPAddr.new(v) }
+    whitelist = ['127.0.0.1', '::1', '62.210.32.0/24', '62.210.33.0/24', '62.210.34.0/24'].map { |v| IPAddr.new(v) }
 
     if whitelist.map { |v| v === IPAddr.new(request.remote_ip) }.include?(true)
       return true
