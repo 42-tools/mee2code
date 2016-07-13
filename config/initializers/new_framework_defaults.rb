92a6f5fc1d9e8fdb93a0e782500f0c06cb1f01b8
Jonathan Philippe <John@MacBook-Pro-de-Jonathan.local>
2016-07-13 12:24:37 +0200

    Correction de la dépendance à coffee-script

--- config/initializers/new_framework_defaults.rb
+++ config/initializers/new_framework_defaults.rb
@@ -30,4 +30,4 @@ Rails.application.config.active_record.belongs_to_required_by_default = false
 # Do not halt callback chains when a callback returns false. Previous versions had true.
 # Rails 5.0 default, so it is introduced as a configuration option to ensure
 # that apps made with earlier versions of Rails are not affected when upgrading.
-ActiveSupport.halt_callback_chains_on_return_false = true
+ActiveSupport.halt_callback_chains_on_return_false = false
